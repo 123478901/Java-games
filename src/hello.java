1e2bbbfd9b97cc1db3db0449e4dfdbcd37424d51
Sebenzile <sebenzilegabone@gmail.com>
2022-06-11 21:21:46 +0200

    Playing around with Java

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,14 @@
 public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+   public static void main(String[] args) {
+        int score ;
+        score = 2;
+        System.out.println("Well mate, Your disappointing score is down below");
+        System.out.println("Very disappointing indead");
+        for(int n=1; n<=2; n++ ){
+            System.out.println(score);
+        }
     }
+  /*   public static void free(String[] args) {
+        System.out.println("I am finally free!!!");
+    }*/
 }
